2b986a445a5eca9d4a675f0b2c3dbc79d2327b76
Pocata <happy80194@gmail.com>
2026-03-25 23:49:03 +0800

    menu/list 樣式細項修正
    [Feat]新增返回按鈕功能、樣式
    [Refactor]輪播菜單樣式修正

--- routes/menu.js
+++ routes/menu.js
@@ -12,10 +12,19 @@ router.get("/list", (req, res) => {
     console.log(err);
   }
 });
-// app.get("/content/:menu", (req, res) => {
-//   !req.query
-//     ? res.send("這是menu輪換圖片頁面")
-//     : res.send("現在在gid=" + req.query.gid + "&pid=" + req.query.pid);
-// });
+router.get("/content", (req, res) => {
+  // !req.query
+  //   ? res.send("這是menu輪換圖片頁面")
+  //   : res.send("現在在gid=" + req.query.gid + "&pid=" + req.query.pid);
+
+  // console.log(req.query.menu);
+  res.render("pages/menu/content", {
+    title: "TASTy西堤牛排",
+    bannerTitle: "MENU",
+    pageBg: "fixBg",
+    cssName: "menu",
+    menu: req.query.menu,
+  });
+});
 
 module.exports = router;
